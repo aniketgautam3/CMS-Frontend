951a2c5da1442ce4fe209745a37e55de66063a4b
debanjandhar10 <debanjand@trainee.nrifintech.com>
2023-02-27 20:42:07 +0530

    jsp

--- src/main/java/com/nrifintech/cmsfrontend/WebConfig.java
+++ src/main/java/com/nrifintech/cmsfrontend/WebConfig.java
@@ -1,17 +1,21 @@
 package com.nrifintech.cmsfrontend;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
  
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/profile/{id}").setViewName("../canteenTemplate.html");
-        registry.addViewController("/dashboard").setViewName("/canteenTemplate.html");
-        registry.addViewController("/dashboard2").setViewName("/canteenTemplate.html");
-        // registry.addViewController("/dashboard3").setViewName("apple");
+        // registry.addViewController("/profile/{id}").setViewName("../canteenTemplate.html");
+        // registry.addViewController("/dashboard").setViewName("/static/canteenTemplate.html");
+        // registry.addViewController("/dashboard2").setViewName("/canteenTemplate.html");
+        registry.addViewController("/dashboard3").setViewName("apple");
     }
+   @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+  }
 }
