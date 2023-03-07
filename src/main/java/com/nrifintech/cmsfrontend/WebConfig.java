af5e7df26030fd37a981ea80e2cf7670703c3869
debanjandhar10 <123241206+debanjandhar10@users.noreply.github.com>
2023-03-07 11:48:53 +0530

    change templates to jsp views

--- src/main/java/com/nrifintech/cmsfrontend/WebConfig.java
+++ src/main/java/com/nrifintech/cmsfrontend/WebConfig.java
@@ -1,19 +1,26 @@
 package com.nrifintech.cmsfrontend;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-    	registry.addViewController("/login").setViewName("/login/index.html");
-        registry.addViewController("/canteen/profile/{id}").setViewName("../../canteenTemplate.html");
-        registry.addViewController("/canteen/orders").setViewName("../canteenTemplate.html");
-        registry.addViewController("/canteen/dashboard").setViewName("../canteenTemplate.html");
-        registry.addViewController("/canteen/menu-management").setViewName("../canteenTemplate.html");
-        registry.addViewController("/user/dashboard").setViewName("../userTemplate.html");
-        registry.addViewController("/admin/dashboard").setViewName("../adminTemplate.html");
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/canteen/profile/{id}").setViewName("canteenTemplate");
+        registry.addViewController("/canteen/orders").setViewName("canteenTemplate");
+        registry.addViewController("/canteen/dashboard").setViewName("canteenTemplate");
+        registry.addViewController("/canteen/menu-management").setViewName("canteenTemplate");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/canteen/**").addResourceLocations("classpath:/static/canteen/");
+        registry.addResourceHandler("/user/**").addResourceLocations("classpath:/static/user/");
+        registry.addResourceHandler("/admin/**").addResourceLocations("classpath:/static/admin/");
     }
 }
--- src/main/webapp/WEB-INF/views/apple.jsp
+++
@@ -1,1 +0,0 @@
-<h1>Hello World from apple</h1>
